3b137edc5b37283f1d766146fce88830a6450404
One'J <OneJ@OneJ.local>
2017-03-11 20:55:28 +0800

    +

---
+++ 动态添加方法/动态添加方法/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  动态添加方法
+//
+//  Created by One'J on 2017/3/11.
+//  Copyright © 2017年 One'J. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
---
+++ 动态添加方法/动态添加方法/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  动态添加方法
+//
+//  Created by One'J on 2017/3/11.
+//  Copyright © 2017年 One'J. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
